35fc37a1aebc91f74a9c35ffba69cf7a14aef832
Dreedle <davis.audrey.lynn@gmail.com>
2015-10-28 09:15:58 -0700

    Revised according to KAri's comments except for the one about relative paths

--- my-site.rb
+++ my-site.rb
@@ -5,4 +5,16 @@ class MySite < Sinatra::Base
   get "/" do
     erb :index
   end
+
+  get "/pangolin" do
+    erb :pangolin
+  end
+
+  get "/dugong" do
+    erb :dugong
+  end
+
+  get "/ffmegabat" do
+    erb :ffmegabat
+  end
 end
